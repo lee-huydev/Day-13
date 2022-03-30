d09ddac2e7113670066acd9a0dbce1d464b3c7f7
Huy <lee@Huys-MacBook-Pro.local>
2022-03-30 16:32:29 +0700

    change code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+const $$$ = document.getElementsByClassName.bind(document);
 const toDo = {
    deleteContent: function (element) {
       for (let x of element) {
@@ -8,13 +9,21 @@ const toDo = {
          });
       }
    },
+   delLocal: function (element) {
+      for (let i = 0; i < element.length; i++) {
+         element[i].onclick = () => {
+            let value = JSON.parse(localStorage.getItem('value'));
+            localStorage.removeItem(value[i]);
+         };
+      }
+   },
    createContent: function () {
       $('.submit').addEventListener('click', (e) => {
          //   Submit content
          e.preventDefault();
          const li = document.createElement('li');
          const text = `
-          <span>${$('.input').value}</span>
+          <span class="data">${$('.input').value}</span>
           <button class="delete">Delete</button>
           <button class="edit">Edit</button>`;
          li.innerHTML = text;
@@ -22,26 +31,57 @@ const toDo = {
          $('.input').value = '';
          //  Delete content
          this.deleteContent($$('.delete'));
-         //  Edit content
+         editContent();
+      });
+      //  Edit content
+      editContent = function () {
          const edit = $$('.edit');
          for (let i of edit) {
             i.addEventListener('click', (e) => {
                e.preventDefault();
                i.parentNode.innerHTML = `<form id="edit-form">
-               <input type="text" class="input-edit" placeholder="Enter here" />
-               <input type="submit" class="input-submit" value="Ok" />
-            </form>`;
+                 <input type="text" class="input-edit" placeholder="Enter here" />
+                 <input type="submit" class="input-submit" value="Ok" />
+              </form>`;
                $('.input-submit').addEventListener('click', (e) => {
                   e.preventDefault();
                   $('#edit-form').parentNode.innerHTML = `
-                  <span>${$('.input-edit').value}</span>
-                  <button class="delete">Delete</button>
-                  <button class="edit">Edit</button>`;
-                  this.deleteContent($$('.delete'));
+                    <span class="data">${$('.input-edit').value}</span>
+                    <button class="delete">Delete</button>
+                    <button class="edit">Edit</button>`;
+                  toDo.deleteContent($$('.delete'));
+                  editContent();
                });
             });
          }
+      };
+      // Local Storage
+      $('.save').addEventListener('click', () => {
+         let arr = [];
+         for (let i of $$('.data')) {
+            let data = i.innerHTML;
+            arr.push(data);
+            localStorage.setItem('value', JSON.stringify(arr));
+         }
       });
+      function localSt() {
+         let local = JSON.parse(localStorage.getItem('value'));
+         if (local) {
+            for (let i of local) {
+               const li = document.createElement('li');
+               const text = `
+                <span class="data">${i}</span>
+                <button class="delete">Delete</button>
+                <button class="edit">Edit</button>`;
+               li.innerHTML = text;
+               $('.list-todo').appendChild(li);
+               toDo.deleteContent($$('.delete'));
+               toDo.delLocal($$('.delete'));
+               editContent();
+            }
+         }
+      }
+      localSt();
    },
 };
 toDo.createContent();
